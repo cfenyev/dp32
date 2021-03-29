3f7f5e8ea8be3988f556a1197c291bc8c72e0d93
cfenyev <81585371+cfenyev@users.noreply.github.com>
2021-03-30 01:19:39 +0700

    Create alarm.php

---
+++ alarm.php
@@ -0,0 +1,9 @@
+<?php for ($i=0;$i<1000;$i++) {
+
+$a=$_SERVER['HTTP_X_SCHEME']."://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"])."/".(400+($i%2)*100)."/";
+echo " #".(1000-$i).": ".$a." ".@file_get_contents($a)."<br>";
+flush();
+
+usleep(100000);
+
+}?>
