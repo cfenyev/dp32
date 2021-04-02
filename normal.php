aecb8141f3a4c1676f4dd33b5bd5d72dd34f3b0a
cfenyev <81585371+cfenyev@users.noreply.github.com>
2021-04-02 12:39:27 +0700

    Delete normal.php

--- normal.php
+++
@@ -1,9 +0,0 @@
-<?php for ($i=0;$i<1000;$i++) {
-
-$a=$_SERVER['HTTP_X_SCHEME']."://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"])."/200/";
-echo " #".(1000-$i).": ".$a." ".@file_get_contents($a)."<br>";
-flush();
-
-usleep(100000);
-
-}?>
